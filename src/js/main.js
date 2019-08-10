e7e13b163eae8e7233e2da381e8d04f7c6a3e5d1
PiotrGrobelak <grobelak.piotr@gmail.com>
2019-08-10 21:59:48 +0200

    feat: add fetch API from gh

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,17 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 
 // place your code below
 
@@ -20,3 +20,43 @@ if ('serviceWorker' in navigator) {
 console.log(`Hello world!`);
 
 
+const repoList = document.querySelector('.projects__cards--js');
+
+fetch('https://api.github.com/users/piotrgrobelak/repos?sort=pushed')
+  .then(response => response.json())
+  .then(response => {
+    const repos = response;
+    for (const repo of repos) {
+      let {
+        name,
+        description,
+        html_url,
+        updated_at,
+        deployments_url,
+        homepage
+      } = repo;
+
+      if (!description) {
+        description = "No description"
+      }
+
+      repoList.innerHTML += `<li class="projects__card">
+      <div class="projects__wrapper">
+        <img class="projects__logo" src="assets/img/icons/githubIcon.svg" alt="Github icon.">
+        <h4 class="projects__name">${name}</h4>
+        <p class="projects__description">${description}</p>
+      </div>
+      <div class="projects__links">
+        <img class="projects__icon" src="assets/img/icons/demoIcon.svg" alt="Link for live project.">
+        <a class="projects__link projects__link--border" href="${html_url}">Demo</a>
+        <img class="projects__icon" src="assets/img/icons/codeIcon.svg" alt="Link for code.">
+        <a class="projects__link" href="${homepage}">Github</a>
+      </div>
+    </li>`
+
+
+
+
+
+    }
+  });
