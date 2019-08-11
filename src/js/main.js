6626286db814db295d9eb7411674e9e681aef604
PiotrGrobelak <grobelak.piotr@gmail.com>
2019-08-11 17:14:04 +0200

    feat: add slide animation to repo cards

--- src/js/main.js
+++ src/js/main.js
@@ -23,14 +23,12 @@ console.log(`Hello world!`);
 const repoList = document.querySelector(".projects__cards--js");
 const moreRepo = document.querySelector(".projects__button--more-js");
 const lessRepo = document.querySelector(".projects__button--less-js");
-
 lessRepo.style.display = "none";
 
 fetch("https://api.github.com/users/piotrgrobelak/repos?sort=pushed")
   .then(response => response.json())
   .then(response => {
     const repos = response;
-
     for (const repo of repos.slice(0, 4)) {
       let {
         name,
@@ -40,24 +38,19 @@ fetch("https://api.github.com/users/piotrgrobelak/repos?sort=pushed")
         deployments_url,
         homepage
       } = repo;
-
-      if (!description) {
-        description = "No description";
-      }
-
-      if (!homepage) {
-        homepage = "";
-      }
-
       repoList.innerHTML += `<li class="projects__card">
       <div class="projects__wrapper">
         <img class="projects__logo" src="assets/img/icons/githubIcon.svg" alt="Github icon.">
         <h4 class="projects__name">${name}</h4>
-        <p class="projects__description">${description}</p>
+        <p class="projects__description">${
+          description ? description : (description = "No description")
+        }</p>
       </div>
       <div class="projects__links">
         <img class="projects__icon" src="assets/img/icons/demoIcon.svg" alt="Link for live project.">
-        <a class="projects__link projects__link--border" href="${homepage}">Demo</a>
+        <a class="projects__link projects__link--border" href="${
+          homepage ? homepage : (homepage = "")
+        }">Demo</a>
         <img class="projects__icon" src="assets/img/icons/codeIcon.svg" alt="Link for code.">
         <a class="projects__link" href="${html_url}">Github</a>
       </div>
@@ -73,27 +66,22 @@ fetch("https://api.github.com/users/piotrgrobelak/repos?sort=pushed")
           deployments_url,
           homepage
         } = repo;
-
-        if (!description) {
-          description = "No description";
-        }
-
-        if (!homepage) {
-          homepage = "";
-        }
         const moreRepoList = document.createElement("li");
-        moreRepoList.classList.add("projects__card");
+        moreRepoList.classList.add("projects__card", "projects__card--slideIn");
         repoList.appendChild(moreRepoList);
-
         moreRepoList.innerHTML += `
       <div class="projects__wrapper">
         <img class="projects__logo" src="assets/img/icons/githubIcon.svg" alt="Github icon.">
         <h4 class="projects__name">${name}</h4>
-        <p class="projects__description">${description}</p>
+        <p class="projects__description">${
+          description ? description : (description = "No description")
+        }</p>
       </div>
       <div class="projects__links">
         <img class="projects__icon" src="assets/img/icons/demoIcon.svg" alt="Link for live project.">
-        <a class="projects__link projects__link--border" href="${homepage}">Demo</a>
+        <a class="projects__link projects__link--border" href="${
+          homepage ? homepage : (homepage = "")
+        }">Demo</a>
         <img class="projects__icon" src="assets/img/icons/codeIcon.svg" alt="Link for code.">
         <a class="projects__link" href="${html_url}">Github</a>
       </div>`;
